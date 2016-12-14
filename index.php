3236442361ba2c95c5ff91159a2db7bf7d5316ec
Albert <w.albert221@gmail.com>
2016-12-14 18:50:39 +0100

    Updated "report bug" URL

--- index.php
+++ index.php
@@ -133,7 +133,7 @@
 					<li><a href="https://www.facebook.com/nastoletnipl">Fanpage</a></li>
 					<li><a href="https://www.facebook.com/groups/NastoletniProgramisci">Grupa</a></li>
 					<li><a href="https://m.me/albert.wolszon">Kontakt</a></li>
-					<li><a href="https://gitlab.com/Albert221/nastoletnitheme/issues" title="Musisz posiadać konto na GitLabie">Zgłoś błąd</a></li>
+					<li><a href="https://github.com/nastoletni/website/issues/new">Zgłoś błąd</a></li>
 					<li><a href="<?php echo admin_url(); ?>">Panel</a></li>
 				</ul>
 			</div>
